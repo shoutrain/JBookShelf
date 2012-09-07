a0e4439f4a06c3a7a3285593257815a881bc3502
Peter Wong <dohkoos@gmail.com>
2012-09-07 22:25:45 +0800

    Struts 2 + Spring 3 + Hibernate 4 login/logout with MySQL

--- src/main/java/name/dohkoos/account/dao/UserDao.java
+++ src/main/java/name/dohkoos/account/dao/UserDao.java
@@ -2,12 +2,19 @@ package name.dohkoos.account.dao;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import name.dohkoos.account.model.User;
 
 public class UserDao {
-    public static User getUser(String username) {
-        Session session = HibernateUtil.getSession();
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public User getUser(String username) {
+        Session session = sessionFactory.openSession();
         try {
             Query query = session.createQuery("from User u where u.username = ?");
             query.setString(0, username);
