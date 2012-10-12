1fc379cb8ce196cc4972e7e5369ff0095f25d05a
Peter Wong <dohkoos@gmail.com>
2012-10-12 23:25:02 +0800

    Added user registration system

---
+++ src/main/java/name/dohkoos/account/service/UserAlreadyExistsException.java
@@ -0,0 +1,21 @@
+package name.dohkoos.account.service;
+
+public class UserAlreadyExistsException extends Exception {
+    private static final long serialVersionUID = -7518737598462419360L;
+
+    public UserAlreadyExistsException() {
+        super();
+    }
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+}
